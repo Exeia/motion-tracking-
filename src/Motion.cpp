0e9746a9805060ec73efddd6c120930c73e9e987
pkeoman17@gmail.com <pkeoman17@gmail.com>
2014-02-21 23:43:17 -0800

    motion works

--- src/Motion.cpp
+++ src/Motion.cpp
@@ -1,13 +1,13 @@
 #include "Motion.h"
 #include "cv.h"
 #include "highgui.h"
+#include<opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
 
 
 Motion::Motion()
 {
-    cap = cvCaptureFromCAM(CV_CAP_ANY);
 }
 
 Motion::~Motion()
@@ -16,21 +16,70 @@ Motion::~Motion()
     cvDestroyWindow("Camera");
 }
 
-void Motion::Init()
+int Motion::Init()
 {
 
+    /*cap = cvCaptureFromCAM(1);
+    if(!cap)
+    {
+        cout<< "Capture failure" <<endl;
+        return -1;
+    }
+
+    IplImage *frame = cvQueryFrame(cap);
+    if (!frame) {
+        return -1;    
+    }
+
+
     char key;
-    printf("Hello world\n");
     cvNamedWindow("Camera",1);
+    cvNamedWindow("Camera1",1);
     while(1){
-        IplImage *frame = cvQueryFrame(cap);
+        
         cvShowImage("Camera", frame);
+        frame = cvQueryFrame(cap);
+        if (!frame) {
+            break;
+        }
+        frame = cvCloneImage(frame);
+
         key = cvWaitKey(10);
         if(char(key) == 27)
         {
             break;
         }
+    }*/
+    Mat frame;
+    Mat back;
+    Mat fore;
+    VideoCapture cap(1);
+    BackgroundSubtractorMOG2 bg;
+    bg.set("nmixtures",3);
+    char key;
+    bg.setBool("detectShadows",false);
+ 
+    vector<std::vector <Point> > contours;
+    cv::namedWindow("Frame");
+    while(true)
+    {
+        cap >> frame;
+        bg.operator ()(frame,fore);
+        bg.getBackgroundImage(back);
+        cv::erode(fore,fore,cv::Mat());
+        cv::dilate(fore,fore,cv::Mat());
+        cv::findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
+        cv::drawContours(frame,contours,-1,cv::Scalar(0,0,255),2);
+        cv::imshow("Frame",frame);
+        cv::imshow("Background",back);
+        key = cvWaitKey(10);
+        if(char(key) == 27)
+        {
+            break;
+        }
+
     }
+    return 0;
 }
 
 void Motion::Tracking()
